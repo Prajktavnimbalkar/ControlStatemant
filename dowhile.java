f4a7dc2920de0a4cfae1976aee9256b0a0436d15
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-01-11 09:09:13 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,14 @@
+package controlstatement;
+
+public class dowhile {
+	public static void main(String[] args) {
+		int i =6;
+		do {
+			System.out.println("i is =" +i);
+			i++;
+		}
+		while(i<5);
+		}
+	}
+
+
