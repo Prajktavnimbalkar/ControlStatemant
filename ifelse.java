63469d4f211d0d4a2f434b7dfa492f40a41843f3
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-01-15 09:03:11 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,14 @@
+package controlstatement;
+
+public class ifelse {
+	public static void main(String[] args) {
+		int num = 7;
+		if (num % 2 ==0) {
+			System.out.println("number is even");
+		}
+		else {
+			System.out.println("number is odd");
+		}
+	}
+
+}
