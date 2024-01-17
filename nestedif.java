4c46537353a83a55614ef565231266f5c01c3c12
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-01-17 09:02:56 +0530

    Add files via upload

---
+++ nestedif.java
@@ -0,0 +1,15 @@
+package controlstatement;
+
+public class nestedif {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		if (x == 11) {
+			System.out.println("bouth values are same");
+		}
+		else {
+			System.out.println("bout are not eqal");
+		}
+	}
+
+}
