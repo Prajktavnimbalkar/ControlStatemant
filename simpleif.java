65ecfeae1aa000de0e410652a5b5868c2066219e
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-01-18 09:02:06 +0530

    Add files via upload

---
+++ simpleif.java
@@ -0,0 +1,14 @@
+package controlstatement;
+
+public class simpleif {
+	public static void main(String[] args) {
+		int a = 9;
+		if (a > 20)
+			System.out.println("a is graterthan 10");
+
+		else {
+			System.out.println("a is lessthan 10");
+		}
+
+	}
+}
