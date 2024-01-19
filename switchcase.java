a15b967c401bf6f6d744eaca2b89846956aff369
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-01-19 09:06:05 +0530

    Add files via upload

---
+++ switchcase.java
@@ -0,0 +1,31 @@
+package controlstatement;
+
+public class switchcase {
+	public static void main(String[] args) {
+		int day = 7;
+		switch (day) {
+		case 1:
+			System.out.println("sunday");
+			break;
+		case 2:
+			System.out.println("monnday");
+			break;
+		case 3:
+			System.out.println("tusday");
+			break;
+		case 4 :
+			System.out.println("wendday");
+			break;
+		case 5:
+			System.out.println("thuesday");
+			break;
+		case 6:
+			System.out.println("friday");
+			break;
+		case 7:
+			System.out.println("saturday");
+			break;
+		}
+	}
+
+}
